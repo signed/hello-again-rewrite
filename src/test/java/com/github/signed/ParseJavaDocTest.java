bef9ff6cf6bf9ca929dfc9c34f3f78802c8a169f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-09-16 20:52:30 +0200

    reproducer

---
+++ src/test/java/com/github/signed/ParseJavaDocTest.java
@@ -0,0 +1,82 @@
+package com.github.signed;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.NlsRewrite.Description;
+import org.openrewrite.NlsRewrite.DisplayName;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.JavadocVisitor;
+import org.openrewrite.java.tree.Javadoc;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import java.util.stream.Collectors;
+
+import static org.openrewrite.java.Assertions.java;
+
+public class ParseJavaDocTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+
+        spec.recipe(new JavadocReproducer());
+    }
+
+
+    @Test
+    void firstLineIsParsedInTwoSeparateText() {
+        rewriteRun(
+          java(
+            """              
+              /**
+               * 2nd line
+               * 3rd line
+               */
+              public class Test {
+              
+              }
+              """));
+    }
+
+    @Value
+    @EqualsAndHashCode(callSuper = false)
+    private static class JavadocReproducer extends Recipe {
+        @Override
+        public @DisplayName String getDisplayName() {
+            return "Javadoc reproducer";
+        }
+
+        @Override
+        public @Description String getDescription() {
+            return "Why split the line into two separate Text.";
+        }
+
+        @Override
+        public TreeVisitor<?, ExecutionContext> getVisitor() {
+            return new JavaVisitor<>() {
+                @Override
+                protected JavadocVisitor<ExecutionContext> getJavadocVisitor() {
+                    return new JavadocVisitor<>(this) {
+                        @Override
+                        public Javadoc visitDocComment(Javadoc.DocComment javadoc, ExecutionContext executionContext) {
+                            var dc = (Javadoc.DocComment) super.visitDocComment(javadoc, executionContext);
+                            System.out.println(dc.getBody().stream().map(it -> {
+                                if (it instanceof Javadoc.Text) {
+                                    return "Javadoc.Text(text=\"" + ((Javadoc.Text) it).getText() + "\")";
+                                } else if (it instanceof Javadoc.LineBreak) {
+                                    return "LineBreak=()";
+                                }
+                                return it.toString();
+                            }).collect(Collectors.joining("\n")));
+                            return dc;
+                        }
+                    };
+                }
+            };
+        }
+    }
+}
